e47a4c036d9a4f1a536be8c6f860a99da9fb9deb
jnthnexpdev <jnthnexpdev@gmail.com>
2025-01-06 19:02:04 -0600

    Interceptor para agregar el token del usuario en cada peticion

---
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { CookiesService } from '../../auth/services/cookies/cookies.service';
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const cookie = inject(CookiesService);
+  const token = cookie.getCookie('session');
+
+  const auth = req.clone({
+    setHeaders : {
+      Authorization : `Bearer ${token}`
+    }
+  });
+
+  return next(auth);
+};
