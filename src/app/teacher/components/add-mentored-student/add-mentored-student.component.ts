e5455285c8d60d60bcdc5ddaa8a51d0e1db78d38
Jonathan Espinosa <jnthnexpdev@gmail.com>
2025-02-07 13:06:09 -0600

    Validacion y registro de asesorados

--- src/app/teacher/components/add-mentored-student/add-mentored-student.component.ts
+++ src/app/teacher/components/add-mentored-student/add-mentored-student.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { AlertService } from '../../../shared/services/alerts/alert.service';
 import { NgClass } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { AdviseService } from '../../../shared/services/advise/advise.service';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormsModule} from '@angular/forms';
 import { Dialog } from '@angular/cdk/dialog';
+import { Router } from '@angular/router';
+
+import { AlertService } from '../../../shared/services/alerts/alert.service';
+import { AdviseService } from '../../../shared/services/advise/advise.service';
+import { Period } from '../../../shared/interfaces/periods.interface';
+import { PeriodService } from '../../../shared/services/periods/period.service';
 
 @Component({
   selector: 'app-add-mentored-student',
   standalone: true,
-  imports: [ NgClass, ReactiveFormsModule ],
+  imports: [ NgClass, ReactiveFormsModule, FormsModule ],
   templateUrl: './add-mentored-student.component.html',
   styleUrl: './add-mentored-student.component.css'
 })
@@ -17,13 +21,16 @@ export class AddMentoredStudentComponent implements OnInit{
   public btnDisable = signal(false);
   public controlNumberInvalid = signal(false);
   public periodInvalid = signal(false);
+  public periods : Period[] = [];
   public registerAdvisedForm !: FormGroup;
 
   constructor(
-    private alertService : AlertService,
-    private formBuilder : FormBuilder,
-    private _adviseService : AdviseService,
     private dialog : Dialog,
+    private formBuilder : FormBuilder,
+    private router : Router,
+    private _adviseService : AdviseService,
+    private _periodService : PeriodService,
+    private _alertService : AlertService,
   ){
     this.registerAdvisedForm = this.formBuilder.group({
       numeroControl : ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]{8,12}$/)]],
@@ -33,7 +40,18 @@ export class AddMentoredStudentComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    
+    this.getPeriodList();
+  }
+
+  private getPeriodList() : void{
+    this._periodService.getPeriodsInfo('').subscribe({
+      next : (response) => {
+        this.periods = response.periods;
+      },
+      error : (err) => {
+        console.error('Error al obtener la lista de periodos: ', err.error.message);
+      }
+    });
   }
 
   private disableBtn() : void{
@@ -74,12 +92,17 @@ export class AddMentoredStudentComponent implements OnInit{
   private sendForm() : void{
     this._adviseService.registerStudentAdvised(this.registerAdvisedForm.value).subscribe({
       next : (response) => {
-        this.alertService.alertOk(response.message, 5000);
+        this._alertService.alertOk(response.message, 3500);
+        setTimeout(() => {
+          this.router.navigate(['/asesor/alumnos-asesorados']).then(() => {
+            window.location.reload()
+          })
+        },3501); 
       },
       error : (err) => {
-        this.alertService.alertError(err.error.message, 5000);
+        this._alertService.alertError(err.error.message, 5000);
       }
-    })
+    });
   }
 
 }
